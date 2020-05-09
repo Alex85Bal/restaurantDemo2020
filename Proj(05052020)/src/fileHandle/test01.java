bfcf4ba6d13edb4bdd55da3806c6ee9a39d0a9c7
Yosi Gliants <yosi213@gmail.com>
2020-05-09 17:15:13 +0300

    TEST

--- Proj(05052020)/src/fileHandle/test01.java
+++ Proj(05052020)/src/fileHandle/test01.java
@@ -18,7 +18,6 @@ public class test01 {
 		System.out.println(dbLink.getReleaseToDB().get(1).asText());
 		System.out.println(dbLink.getReleaseToDB().get(2).asText());
 	
-		System.out.println("CHECKING HELLO!!!");
-		System.out.println("please work!!");
+		System.out.println("SUP");
 	}
 }
