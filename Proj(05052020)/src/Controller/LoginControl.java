f6d0e417c555f23a9b529543d09545b814e07468
Alex85Bal <65068060+Alex85Bal@users.noreply.github.com>
2020-07-02 20:46:27 +0300

    begining of controller Idea

---
+++ Proj(05052020)/src/Controller/LoginControl.java
@@ -0,0 +1,15 @@
+package Controller;
+
+public class LoginControl {
+	
+	private boolean isExist;
+	private String PID;
+	private String Pass;
+	private String UID; 
+	
+	public LoginControl()
+	{
+		
+	}
+
+}
