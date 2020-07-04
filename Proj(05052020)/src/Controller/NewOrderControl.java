7dfa3a10a0cde4a8a4bca72932811d7644795d63
Alex85Bal <65068060+Alex85Bal@users.noreply.github.com>
2020-07-04 21:00:06 +0300

    another controllers update, UI sude of controllers should be done (0.0)

--- Proj(05052020)/src/Controller/NewOrderControl.java
+++ Proj(05052020)/src/Controller/NewOrderControl.java
@@ -8,6 +8,7 @@ import javax.swing.JFrame;
 import GUI.goBackEvent;
 import GUI.incomingOrderEvent;
 import GUI.main_cardFrame;
+import GUI.user_obj;
 
 public class NewOrderControl extends Observable implements Observer {
 	
@@ -15,8 +16,8 @@ public class NewOrderControl extends Observable implements Observer {
 	private String[] servableDishes;
 	private boolean goBack = false;
 	
-	public NewOrderControl() {
-		orders = new main_cardFrame();
+	public NewOrderControl(user_obj user) {
+		orders = new main_cardFrame(user);
 		
 		if (orders != null) {
 			orders.addObserver(this);
@@ -24,7 +25,7 @@ public class NewOrderControl extends Observable implements Observer {
 			//Module --> dishes that can be served at this point of time
 			servableDishes = new String[] {"Tofu Soup", "Soy-milk Flan", "Rice Stir-fry with Veggies"};
 			System.out.println("in NewOrderControl , requesting servable dishes from the Model");
-			orders.displayNewOrderScreen(servableDishes, servableDishes.length);
+			orders.displayNewOrderScreen(servableDishes, servableDishes.length, user);
 			System.out.println("in NewOrderControl , launching new orders screen");
 		}
 	}
@@ -35,19 +36,25 @@ public class NewOrderControl extends Observable implements Observer {
 		if (arg.getClass() == new incomingOrderEvent(new JFrame()).getClass() ) {
 			System.out.println("in NewOrderControl , user event: --> new order");
 			incomingOrderEvent temp = (incomingOrderEvent) arg;
-			// Model <-- validate incoming order;
-			// Model --> order is valid / order invalid + amount that can be served
-			boolean Model = true;
+			// Yosi, an example of how i built the incoming order event class: 
+			//temp.getStaff_id();
+			//temp.getTable_id();
+			//temp.getOrders(); // <-- a List<order_item>
+			//temp.getOrders().get(0).getAmount();
+			//temp.getOrders().get(0).getDish();
+			
+			// Model <-- validate incoming order , check if dish ingredients exist in inventory 
+			// Model --> order is valid / order invalid + amount that can be served or any error msg
+			boolean Model = false;
 			
 			if (Model) {// Model says yes
-				
+				System.out.println("in NewOrderControl , Model approves order");
+				orders.displayOrderSucess();
 			}
 			else {// Model says no
+				System.out.println("in NewOrderControl , Model dis-approves order");
 				orders.displayInvalidAmount(new String[] {"1 tofu","2 soy milk"});
 			}
-
-			
-			//orders.displayInvalidAmount();
 		}
 			
 		if (arg instanceof Boolean) {
@@ -57,7 +64,5 @@ public class NewOrderControl extends Observable implements Observer {
 			setChanged();
 			notifyObservers(goBack);
 		}
-		
 	}
-	
 }
--- Proj(05052020)/src/Controller/OrderServedControl.java
+++ Proj(05052020)/src/Controller/OrderServedControl.java
@@ -1,5 +1,58 @@
 package Controller;
 
-public class OrderServedControl {
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Vector;
 
+import javax.swing.JPanel;
+
+import GUI.goBackEvent;
+import GUI.main_cardFrame;
+import GUI.readyDish_item;
+import GUI.user_obj;
+
+public class OrderServedControl extends Observable implements Observer{
+	
+	private main_cardFrame ready;
+	private boolean goBack = false;
+	private user_obj user;
+	private Vector<readyDish_item> shouldBeServed;
+	
+	public OrderServedControl(user_obj user) {
+		
+		this.user = user;
+		shouldBeServed = new Vector<readyDish_item>();
+		ready = new main_cardFrame(user);
+		ready.addObserver(this);
+		// Module <-- request for Ready dishes for user.
+		// Module --> partial  OR whole Order.
+		// i'm faking it ^ :
+		shouldBeServed.add(new readyDish_item("1",String.valueOf(user.getSystem_id()), "carrot cake", "2", ""));
+		shouldBeServed.add(new readyDish_item("1",String.valueOf(user.getSystem_id()), "died coke", "1", ""));
+		// end of faking.
+		ready.displayReadyOrderScreen(shouldBeServed);
+	}
+	
+	public void backToReadyOrders () {
+		ready.flipVisibility();
+		// Module <-- request for Ready dishes for user.
+		// Module --> partial  OR whole Order.
+		// i'm faking it:
+		shouldBeServed.add(new readyDish_item("6",String.valueOf(user.getSystem_id()), "Strong Green-tea", "1", ""));
+		ready.displayReadyOrderScreen(shouldBeServed);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if (arg.getClass() == new Vector<readyDish_item>().getClass() ) {
+			Vector<readyDish_item> temp = (Vector<readyDish_item>) arg;
+			for (readyDish_item readyDish_item : temp) {
+				System.out.println(readyDish_item.getName()+" "+readyDish_item.getAmount()+" Left to serve");
+			}
+			goBack = true;
+			ready.flipVisibility();
+			setChanged();
+			notifyObservers(goBack);
+		}
+	}
 }
