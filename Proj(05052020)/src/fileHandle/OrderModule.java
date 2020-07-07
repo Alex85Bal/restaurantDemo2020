53883c28646a28905d4b60574b09f828044b6d26
Alex85Bal <65068060+Alex85Bal@users.noreply.github.com>
2020-07-07 23:51:23 +0300

    varify Order to file logic

--- Proj(05052020)/src/fileHandle/OrderModule.java
+++ Proj(05052020)/src/fileHandle/OrderModule.java
@@ -4,6 +4,8 @@ import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -21,18 +23,30 @@ import GUI.order_item;
 public class OrderModule {
 	
 	private Vector<dataBaseItem> CurrentInventory;
+	private Vector<dataBaseItem> UpdatedInventory;
+	
 	private Vector<dataBaseItem> CurrentDishes;
-	private HashMap<Integer, Integer> ReverseUpdates;
+	private HashMap<Integer, Integer> reverseInvChanges;
+	private HashMap<Integer, Integer> reverseUpdate;
+	
 	private HashMap<String, Integer> Approved;
 	private boolean reloadrequired = false;
 	private dataBaseItemTest Stream;
 	private String errorMsg;
+	private DateTimeFormatter dtf;
+	private LocalDateTime now;
 	
 	public OrderModule() {
 		
+		dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		now = LocalDateTime.now();  
+		System.out.println(dtf.format(now));  
+		
 		Stream = new dataBaseItemTest();
 		CurrentInventory = new Vector<dataBaseItem>();
-		ReverseUpdates = new HashMap<Integer, Integer>();
+		UpdatedInventory = new Vector<dataBaseItem>();
+		reverseInvChanges = new HashMap<Integer, Integer>();
+		reverseUpdate = new HashMap<Integer, Integer>();
 		Approved = new HashMap<String, Integer>();
 	}
 	
@@ -63,18 +77,32 @@ public class OrderModule {
 		}
 	}
 	
-	public String incomingOrder (incomingOrderEvent IncomingOrder) {
+	private boolean loadUpdatedInventory () {
+		try {
+			
+			Stream.readFromFile("C:\\projects", "inventory@1.txt");
+			UpdatedInventory = Stream.whatFileToAccess("inventoryItem", Stream);
+			return true;
+			
+		} catch (Exception e) {
+			return false;
+		}
 		
+	}
+	
+	public String incomingOrder (incomingOrderEvent IncomingOrder) {
 		
 		ChangeableString canOrderOnlyThose = new ChangeableString("");
 		//displayInventory();
-		ReverseUpdates.clear();
+		reverseInvChanges.clear();
 		Approved.clear();
 		if (CurrentInventory.isEmpty()) {
 			return "0 inventory";
 		}
 		if (isOrderAllowed(IncomingOrder, canOrderOnlyThose)) {
 			// write to CurrentInventory to inventory file.
+			loadUpdatedInventory();
+			fileVsInventoryCheck();
 			return "";
 		}
 		else return canOrderOnlyThose.getIt();
@@ -110,7 +138,7 @@ public class OrderModule {
 			}
 			
 			if (!greenLight) {
-				reloadrequired =  !unfuckInventory(ReverseUpdates);
+				reloadrequired =  !unfuckInventory(reverseInvChanges, CurrentInventory);
 				output.changeTo(whatCanBeRequested());
 			}
 			return greenLight;
@@ -139,7 +167,7 @@ public class OrderModule {
 				if ( ing.getID() == temp.getID() ) {
 					if (temp.getCurrentStock() >= amount) {
 						if(reduceInventory) {
-							ReverseUpdates.putIfAbsent(i, temp.getCurrentStock());
+							reverseInvChanges.putIfAbsent(i, temp.getCurrentStock());
 							temp.setCurrentStock(temp.getCurrentStock() - amount);
 							return false;
 						}
@@ -154,16 +182,16 @@ public class OrderModule {
 		}
 	}
 	
-	private boolean unfuckInventory (HashMap<Integer, Integer> fixer) {
+	private boolean unfuckInventory (HashMap<Integer, Integer> fixer, Vector<dataBaseItem> unfuck) {
 		
 		try {
 			Set mapStart = fixer.entrySet(); // binds the map into sets 
 			Iterator iterator = mapStart.iterator(); // allows iteration
 			while(iterator.hasNext()) {
 				Map.Entry mapSet = (Map.Entry)iterator.next();
-				inventoryItem temp = (inventoryItem) CurrentInventory.get((int) mapSet.getKey());
+				inventoryItem temp = (inventoryItem) unfuck.get((int) mapSet.getKey());
 				temp.setCurrentStock((int) mapSet.getValue());
-				CurrentInventory.set((int) mapSet.getKey(), temp);
+				unfuck.set((int) mapSet.getKey(), temp);
 			}
 			return true;
 			
@@ -202,28 +230,77 @@ public class OrderModule {
 			return "Error_OrdMod";
 		}
 	}
+	
+	public void hasMyInventoryExpired () {
+		// date <-- first line of inventoryfile
+		// if date > myDate --> update(UpdatedInventory) + return true + writeToFile(lockFile)
+		// only a partial solution:
+		// else return false
+	}
+	
+	public boolean fileVsInventoryCheck() {
+		try {
+			//if(UpdatedInventory.isEmpty()) return false;
+			Set mapStart = reverseInvChanges.entrySet(); // binds the map into sets 
+			Iterator iterator = mapStart.iterator(); // allows iteration
+			while(iterator.hasNext()) {
+				Map.Entry mapSet = (Map.Entry)iterator.next();
+				inventoryItem myItem = (inventoryItem) CurrentInventory.get((int) mapSet.getKey());
+				inventoryItem updatedItem = (inventoryItem) UpdatedInventory.get((int) mapSet.getKey());
+				//CurrentInventory.set((int) mapSet.getKey(), temp);
+				//System.out.println(myItem.getItemName()+" : INDEX --> "+(int) mapSet.getKey());
+				//System.out.println(updatedItem.getItemName()+" : INDES --> "+(int) mapSet.getKey());
+				int delta = Math.abs( (int)mapSet.getValue() - myItem.getCurrentStock());
+				System.out.println("value Before local order "+(int)mapSet.getValue());
+				System.out.println("currently in stock "+updatedItem.getCurrentStock());
+				if( delta <= updatedItem.getCurrentStock() ) {
 
-/*
+					reverseUpdate.put((int) mapSet.getKey(), updatedItem.getCurrentStock());
+					int newStock = updatedItem.getCurrentStock() - delta;
+					updatedItem.setCurrentStock(newStock);
+					UpdatedInventory.set((int) mapSet.getKey(), updatedItem);
+				}
+				else {
+					
+					unfuckInventory(reverseUpdate, UpdatedInventory);
+					return false;
+				}
+			}
+			return true;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+	
 public static void main(String[] args) throws IOException, Exception  {
 		
 		OrderModule tes = new OrderModule();
 		tes.loadDishes();
 		tes.loadInventory();
-		String results = "";
+		tes.loadUpdatedInventory();
 		
-		Vector<dishItem> dishes = tes.getDishesh();
-
+		inventoryItem now;
+		inventoryItem upd;
 		
-		List<order_item> myDishes = new Vector<order_item>();
-		myDishes.add(new order_item("test1", 5, dishes.get(0)));
-		myDishes.add(new order_item("test2", 11, dishes.get(1)));
-		incomingOrderEvent ord1 = new incomingOrderEvent(new JPanel(), myDishes, 666);
+		// lets diminish the updated inventory in 2 items !
+		upd = (inventoryItem) tes.UpdatedInventory.get(0);
+		upd.setCurrentStock(10); // we have 10 potato left! in the "file"
+		tes.UpdatedInventory.set(0, upd);
 		
-		//tes.displayInventory();
-		results = tes.incomingOrder(ord1);
-		System.out.println( results == "" ? "Order Accepted" : "Order Refused\n"+results+"\nwill be accepted");
-		System.out.println("\n");
-		//tes.displayInventory();
-	}
-*/
+		upd = (inventoryItem) tes.UpdatedInventory.get(1);
+		upd.setCurrentStock(0);
+		tes.UpdatedInventory.set(1, upd);
+			
+		// lets fake an dish order that used potato and chicken
+		tes.reverseInvChanges.put(0, 210); // index 0 is the potato 10 is the amount that was before the order
+		tes.reverseInvChanges.put(1,18);
+			
+		tes.fileVsInventoryCheck();
 }
+
+}
+
+
+
+
