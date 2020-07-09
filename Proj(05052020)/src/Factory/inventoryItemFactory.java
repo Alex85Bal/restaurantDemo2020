150f22d1d5a28976285283c3e725584f1f9fd8b4
Yosi Gliants <yosi213@gmail.com>
2020-07-09 14:36:14 +0300

    last one i dont promise

--- Proj(05052020)/src/Factory/inventoryItemFactory.java
+++ Proj(05052020)/src/Factory/inventoryItemFactory.java
@@ -6,13 +6,25 @@ import fileHandle.inventoryItem;
 public class inventoryItemFactory extends dataBaseItemFactory {
 	
 	public inventoryItem createInventoryItemObject(String DBN,int item_ID, String item_name, int branchID, int stock,int minStock,String type,int req)	{
-		if (req == 1)return new inventoryItem(DBN, item_ID, item_name, branchID, stock, minStock, type);
-		if (req == 2)return new inventoryItem(DBN);
+		if (req == 7)return new inventoryItem(DBN, item_ID, item_name, branchID, stock, minStock, type);
+		if (req == 1)return new inventoryItem(DBN);
+		if (req == 0) return new inventoryItem();
 		else return null;
 	}
 
+	
+	public inventoryItem createEmtpyICtor() {
+		return new inventoryItem();
+	}
+	
 	@Override
 	public dataBaseItem createEmptyCtor() {
+		return new inventoryItem();
+	}
+
+
+	@Override
+	public dataBaseItem createObject() {
 		return new inventoryItem();
 	}
 }
