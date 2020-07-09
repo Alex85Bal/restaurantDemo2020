0e106a0ff24be88a07de111e83624b2cfeb86c85
Yosi Gliants <yosi213@gmail.com>
2020-07-09 14:34:47 +0300

    hi

--- Proj(05052020)/src/Factory/workerFactory.java
+++ Proj(05052020)/src/Factory/workerFactory.java
@@ -12,12 +12,23 @@ import fileHandle.inventoryItem;
 public class workerFactory extends dataBaseItemFactory {
 
 	public Workers createWorkerItemObject(String DBN,int id,String Name, int branch, ArrayList<Integer> auth,String pass,String Type,int req) {
-		if (req == 1) return new Workers(DBN,id, Name,branch, auth,pass,Type);
-		if (req == 2) return new Workers(DBN);
+	 if (req == 2)return new Workers(DBN,id, Name,branch, auth,pass,Type);
+		if (req == 1) return new Workers(DBN);
+		if (req == 0) return new Workers();
 		else return null;
 	}
+	
+	public Workers createEmptyWCtor() {
+		return new Workers();
+	}
+
 	@Override
 	public dataBaseItem createEmptyCtor() {
+		return new Workers();
+	}
+
+	@Override
+	public dataBaseItem createObject() {
 		return new Workers();
 	}
 
